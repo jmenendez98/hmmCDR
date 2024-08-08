bc41bcb9f2fdf178d43a7b858683d7553fdff4e3
Julian Menendez <jmmenend@ucsc.edu>
2024-08-07 19:59:23 -0700

    editted the hmmCDR_priors file to be able to run independantly. Tested it on chr10_MAT inputs

--- hmmCDR/hmmCDR_priors.py
+++ hmmCDR/hmmCDR_priors.py
@@ -4,87 +4,38 @@ import pybedtools
 import argparse
 import os
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Process bedMethyl and CenSat BED file to produce hmmCDR priors')
-    
-    # Required arguments
-    parser.add_argument('bedMethyl_path', type=str, help='Path to the bedMethyl file')
-    parser.add_argument('cenSat_path', type=str, help='Path to the CenSat BED file')
-    parser.add_argument('output_path', type=str, help='Path to the output priorCDRs BED file')
-    
-    # Optional arguments with default values
-    parser.add_argument('-m', '--mod_code', type=str, default='m', help='Modification code to filter bedMethyl file (default: "m")')
-    parser.add_argument('-s', '--sat_type', type=str, default='H1L', help='Satellite type/name to filter CenSat bed file. (default: "H1L")')
-    parser.add_argument('-w', '--window_size', type=int, default=1020, help='Window size to calculate prior regions. (default: 1020)')
-    parser.add_argument('--priorCDR_percent', type=int, default=5, help='Percentile for finding priorCDR regions. (default: 5)')
-    parser.add_argument('--priorTransition_percent', type=int, default=10, help='Percentile for finding priorTransition regions. (default: 10)')
-    parser.add_argument('--minCDR_size', type=int, default=3000, help='Minimum size for CDR regions. (default: 3000)')
-    parser.add_argument('--depletion', action='store_true', default=True, help='Depletion flag. Set to False if you are looking for enriched regions. (default: True)')
-    parser.add_argument('--save_intermediates', action='store_true', default=False, help="Set to true if you would like to save intermediates(filtered beds+window means). (default: False)")
-    parser.add_argument('--output_label', type=str, default='CDR', help='Label to use for name column of priorCDR BED file. (default: "CDR")')
-    
-    args = parser.parse_args()
-    return args
+from hmmCDR_parser import hmmCDR_parser
 
 class hmmCDR_priors:
-    def __init__(self, bedMethyl_path, cenSat_path, output_path, mod_code='m', sat_type='H1L', 
-                 window_size=1020, priorCDR_percent=5, priorTransition_percent=10, 
-                 minCDR_size=3000, depletion=True, output_label='CDR', 
+    def __init__(self, bedgraphMethyl, output_path, 
+                 window_size=1020, minCDR_size=3000,
+                 priorCDR_percent=5, priorTransition_percent=10, 
+                 output_label='CDR', enrichment=False,
                  save_intermediates=False):
         
         # required
-        self.bedMethyl_path = bedMethyl_path
-        self.cenSat_path = cenSat_path
+        self.bedgraphMethyl = bedgraphMethyl
         self.output_path = output_path
 
         # optionally changed
-        self.mod_code = mod_code
-        self.sat_type = sat_type
         self.window_size = window_size
         self.minCDR_size = minCDR_size
         self.priorCDR_percent = priorCDR_percent
         self.priorTransition_percent = priorTransition_percent
-        self.depletion = depletion
+        self.enrichment = enrichment
         self.output_label = output_label
         self.save_intermediates=save_intermediates
 
         # calculated
-        filtered_bedMethyl = self.filter_bedMethyl()
-        filtered_regions = self.filter_regions()
-        intersected_bedMethyl = self.intersect_files(filtered_bedMethyl, filtered_regions)
-        windows = self.create_windows(intersected_bedMethyl)
-        windows_mean = self.mean_within_windows(intersected_bedMethyl, windows)
-        cdr_score, transition_score = self.calculate_percentiles(windows_mean)
+        windows = self.create_windows(self.bedgraphMethyl)
+        windows_mean = self.mean_within_windows(self.bedgraphMethyl, windows)
+        self.cdr_score, self.transition_score = self.calculate_percentiles(windows_mean)
 
-        if save_intermediates:
-            base_output_path = os.path.splitext(self.output_path)[0]
-            windows_mean.to_csv(f'{base_output_path}_windows_mean.bedgraph', sep='\t', index=False, header=False)
-            filtered_bedMethyl.to_csv(f'{base_output_path}_filtered_bedMethyl.bedgraph', sep='\t', index=False, header=False)
-            filtered_regions.to_csv(f'{base_output_path}_filtered_regions.bed', sep='\t', index=False, header=False)
+        self.priorCDR = self.create_priorCDR_dataframe(windows_mean, self.cdr_score)
+        self.priorTranstion = self.create_priorTransition_dataframe(windows_mean, self.transition_score, self.priorCDR)
 
-        self.priorCDR = self.create_priorCDR_dataframe(windows_mean, cdr_score)
-        self.priorTranstion = self.create_priorTransition_dataframe(windows_mean, transition_score, self.priorCDR)
-
-        self.priorsBed = self.combine_beds(self.priorCDR, self.priorTranstion)
+        self.hmmCDRpriors = self.combine_beds(self.priorCDR, self.priorTranstion)
         
-
-    def filter_bedMethyl(self):
-        df = pd.read_csv(self.bedMethyl_path, sep='\t', header=None, usecols=[0, 1, 2, 3, 10])
-        filtered_df = df[df[3] == self.mod_code]
-        filtered_df = filtered_df.drop(columns=[3])
-        return filtered_df
-
-    def filter_regions(self):
-        regions_df = pd.read_csv(self.cenSat_path, sep='\t', header=None)
-        filtered_regions_df = regions_df[regions_df[3].str.contains(self.sat_type)]
-        return filtered_regions_df
-
-    def intersect_files(self, filtered_bedMethyl, filtered_regions):
-        bedMethyl_bedtool = pybedtools.BedTool.from_dataframe(filtered_bedMethyl)
-        regions_bedtool = pybedtools.BedTool.from_dataframe(filtered_regions)
-        intersected = bedMethyl_bedtool.intersect(regions_bedtool, wa=True, u=True)
-        intersected_df = intersected.to_dataframe(names=[0, 1, 2, 3])
-        return intersected_df
 
     def create_windows(self, intersected_bedMethyl):
         min_val = int(intersected_bedMethyl[1].min())
@@ -115,7 +66,7 @@ class hmmCDR_priors:
     def create_priorCDR_dataframe(self, windows_mean_df, priorCDR_score):
         windows_mean_df['mean_value'] = pd.to_numeric(windows_mean_df['mean_value'], errors='coerce')
         windows_mean_df = windows_mean_df.dropna(subset=['mean_value'])
-        if not self.depletion:
+        if self.enrichment:
             windows_below_priorCDR_score = windows_mean_df[windows_mean_df['mean_value'] > priorCDR_score]
         else:
             windows_below_priorCDR_score = windows_mean_df[windows_mean_df['mean_value'] < priorCDR_score]
@@ -130,7 +81,7 @@ class hmmCDR_priors:
     def create_priorTransition_dataframe(self, windows_mean_df, priorTransition_score, CDR_df):
         windows_mean_df['mean_value'] = pd.to_numeric(windows_mean_df['mean_value'], errors='coerce')
         windows_mean_df = windows_mean_df.dropna(subset=['mean_value'])
-        if not self.depletion:
+        if self.enrichment:
             windows_below_priorTransition_score = windows_mean_df[windows_mean_df['mean_value'] > priorTransition_score]
         else:
             windows_below_priorTransition_score = windows_mean_df[windows_mean_df['mean_value'] < priorTransition_score]
@@ -154,24 +105,48 @@ class hmmCDR_priors:
 
 
 def main():
-    args = parse_args()
+    parser = argparse.ArgumentParser(description='Process bedMethyl and CenSat BED file to produce hmmCDR priors')
+    
+    # Required arguments
+    parser.add_argument('bedMethyl_path', type=str, help='Path to the bedMethyl file')
+    parser.add_argument('cenSat_path', type=str, help='Path to the CenSat BED file')
+    parser.add_argument('output_path', type=str, help='Path to the output priorCDRs BED file')
+    
+    # Optional arguments with default values
+    parser.add_argument('-m', '--mod_code', type=str, default='m', help='Modification code to filter bedMethyl file (default: "m")')
+    parser.add_argument('-s', '--sat_type', type=str, default='H1L', help='Satellite type/name to filter CenSat bed file. (default: "H1L")')
+    parser.add_argument('--bedgraph', action='store_true', help='Flag indicating if the input is a bedgraph. (default: False)')
+    parser.add_argument('-w', '--window_size', type=int, default=1020, help='Window size to calculate prior regions. (default: 1020)')
+    parser.add_argument('--priorCDR_percent', type=int, default=5, help='Percentile for finding priorCDR regions. (default: 5)')
+    parser.add_argument('--priorTransition_percent', type=int, default=10, help='Percentile for finding priorTransition regions. (default: 10)')
+    parser.add_argument('--minCDR_size', type=int, default=3000, help='Minimum size for CDR regions. (default: 3000)')
+    parser.add_argument('--enrichment', action='store_true', help='Enrichment flag. Pass in if you are looking for methylation enriched regions. (default: False)')
+    parser.add_argument('--save_intermediates', action='store_true', default=False, help="Set to true if you would like to save intermediates(filtered beds+window means). (default: False)")
+    parser.add_argument('--output_label', type=str, default='CDR', help='Label to use for name column of priorCDR BED file. (default: "CDR")')
+    
+    args = parser.parse_args()
 
-    priorCDR = hmmCDR_priors(
+    CDRparser = hmmCDR_parser(
         bedMethyl_path=args.bedMethyl_path,
         cenSat_path=args.cenSat_path,
-        output_path=args.output_path,
         mod_code=args.mod_code,
         sat_type=args.sat_type,
+        bedgraph=args.bedgraph
+    )
+
+    CDRpriors = hmmCDR_priors(
+        bedgraphMethyl=CDRparser.subset_bedgraphMethyl,
+        output_path=args.output_path,
         window_size=args.window_size,
+        minCDR_size=args.minCDR_size,
         priorCDR_percent=args.priorCDR_percent,
         priorTransition_percent=args.priorTransition_percent,
-        minCDR_size=args.minCDR_size,
-        depletion=args.depletion,
+        enrichment=args.enrichment,
         save_intermediates=args.save_intermediates,
         output_label=args.output_label
     )
 
-    priorCDR.priorsBed.to_csv(args.output_path, sep='\t', header=False, index=False)
+    CDRpriors.hmmCDRpriors.to_csv(args.output_path, sep='\t', header=False, index=False)
 
 if __name__ == "__main__":
     main()
