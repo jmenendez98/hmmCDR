b2724be2ae2c7050a98fbead95e2fd982f22ee98
jmenendez98 <jmmenend@ucsc.edu>
2024-08-30 15:09:32 -0700

    edited versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmmCDR',
-    version='0.1.0',
+    version='0.1.1',
     description="Find CDR locations using bedmethyl file and CenSat annotations.(REQUIRES BEDTOOLS INSTALLED)",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
