86636dea1f1078a3be4c6a0d0d474ed4417a262d
jmenendez98 <jmmenend@ucsc.edu>
2024-08-30 14:17:36 -0700

    Edited pypi dependancies

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmmCDR',
-    version='1.0.0',
-    description="Using pybedtools and hmmlearn to find precise locations of CDRs and subCDRs from bedMethyl and CenSat annotation files.",
+    version='0.1.0',
+    description="Find CDR locations using bedmethyl file and CenSat annotations.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/jmenendez98/hmmCDR",
@@ -12,10 +12,10 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'pandas',
-        'pybedtools',
-        'hmmlearn'
+        'numpy>=1.21.5',
+        'pandas>=1.3.5',
+        'pybedtools>=0.8.1',
+        'hmmlearn>=0.3.0'
     ],
     entry_points={
         'console_scripts': [
