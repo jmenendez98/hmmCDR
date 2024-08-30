6fdac2f9eca5d73a1c6fc1810b28fac62abb88ff
jmenendez98 <jmmenend@ucsc.edu>
2024-08-30 15:39:30 -0700

    Updated versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmmCDR',
-    version='0.1.1',
+    version='0.1.2',
     description="Find CDR locations using bedmethyl file and CenSat annotations.(REQUIRES BEDTOOLS INSTALLED)",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -11,16 +11,20 @@ setup(
     author_email='jmmenend@ucsc.edu',
     license="MIT",
     packages=find_packages(),
+    project_urls={
+        'GitHub': 'https://github.com/jmenendez98/hmmCDR',
+        'Lab Website': 'https://migalab.com/',
+    },
     install_requires=[
         'numpy>=1.21.5',
         'pandas>=1.3.5',
         'pybedtools>=0.8.1',
-        'hmmlearn>=0.3.0'
+        'hmmlearn>=0.3.0',
     ],
     entry_points={
         'console_scripts': [
             'hmmCDR=hmmCDR.hmmCDR:main',
-            'hmmCDRprior=hmmCDR.hmmCDRprior:main'
+            'hmmCDRprior=hmmCDR.hmmCDRprior:main',
         ]
     },
     classifiers=[
