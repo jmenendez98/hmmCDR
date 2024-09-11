e11556e46c530c82e3c16c952273362ea0a25de2
jmenendez98 <jmmenend@ucsc.edu>
2024-09-10 19:02:14 -0700

    Fixed big in retrying if no subCDRs are found

--- hmmCDR/hmmCDRprior.py
+++ hmmCDR/hmmCDRprior.py
@@ -8,14 +8,13 @@ import concurrent.futures
 from hmmCDR.hmmCDRparse import hmmCDRparse
 
 class hmmCDRprior:
-    def __init__(self, merge_distance, window_size, min_size, 
-                 prior_percent, prior_transition_percent, enrichment, output_label):
+    def __init__(self, merge_distance, window_size, min_size, prior_percentile,  enrichment, output_label):
         self.merge_distance = merge_distance
         self.window_size = window_size
         self.min_size = min_size
 
-        self.prior_percent = prior_percent
-        self.prior_transition_percent = prior_transition_percent
+        self.prior_percentile = prior_percentile
+
         self.enrichment = enrichment
         self.output_label = output_label
         
@@ -37,15 +36,11 @@ class hmmCDRprior:
 
     def calculate_prior_percentiles(self, window_means):
         mean_values = pd.to_numeric(window_means['mean_value'].replace('.', np.nan)).dropna()
-        return (np.percentile(mean_values, self.prior_percent),
-                np.percentile(mean_values, self.prior_transition_percent))
+        return ( np.percentile(mean_values, self.prior_percentile) )
 
-    def create_priorCDR_dataframe(self, merge_distance, windows_mean_df, priorCDR_score):
-        # Ensure 'mean_value' is numeric and drop NaN values
+    def create_prior_dataframe(self, merge_distance, windows_mean_df, priorCDR_score):
         windows_mean_df['mean_value'] = pd.to_numeric(windows_mean_df['mean_value'], errors='coerce')
         windows_mean_df = windows_mean_df.dropna(subset=['mean_value'])
-
-        # Apply the correct condition based on the enrichment flag
         if self.enrichment:
             condition = windows_mean_df['mean_value'] > priorCDR_score
         else:
@@ -53,21 +48,14 @@ class hmmCDRprior:
 
         priorCDR_windows = windows_mean_df[condition]
 
-        # Merge and filter regions based on size
         merged_windows = pybedtools.BedTool.from_dataframe(priorCDR_windows).merge(d=merge_distance)
-        merged_df = merged_windows.to_dataframe(names=[0, 1, 2])
+        merged_df = merged_windows.to_dataframe()
 
-        # Filter regions by minimum size
-        return merged_df[merged_df[2] - merged_df[1] >= self.min_size]
+        if merged_df.empty:
+            return pd.DataFrame()
+        
+        return merged_df[merged_df['end'] - merged_df['start'] >= self.min_size]
 
-    def create_priorTransition_dataframe(self, windows_mean_df, priorTransition_score, CDR_df):
-        condition = windows_mean_df['mean_value'] > priorTransition_score if self.enrichment else windows_mean_df['mean_value'] < priorTransition_score
-        windows_below = windows_mean_df[condition].dropna(subset=['mean_value'])
-        
-        intersected_df = pybedtools.BedTool.from_dataframe(windows_below).merge().intersect(pybedtools.BedTool.from_dataframe(CDR_df), wa=True, wb=True).to_dataframe(names=[0, 1, 2, 'CDR_1', 'CDR_2', 'CDR_3', 'CDR_4'])[[0, 1, 2]]
-        
-        return pybedtools.BedTool.from_dataframe(intersected_df).subtract(pybedtools.BedTool.from_dataframe(CDR_df)).to_dataframe(names=[0, 1, 2])
-    
     def combine_beds(self, priorCDR_df, priorTransition_df):
         priorCDR_df[3], priorTransition_df[3] = self.output_label, f"{self.output_label}_transition"
         return pd.concat([priorCDR_df, priorTransition_df], ignore_index=True).sort_values(by=1).reset_index(drop=True)
@@ -75,24 +63,23 @@ class hmmCDRprior:
     def priors_single_chromosome(self, chrom, bed4Methyl_chrom):
         windows = self.create_windows(bed4Methyl_chrom)
         windows_mean = self.mean_within_windows(bed4Methyl_chrom, windows)
-        cdr_score, transition_score = self.calculate_prior_percentiles(windows_mean)
-        priorCDRs = self.create_priorCDR_dataframe(self.merge_distance, windows_mean, cdr_score)
-        priorTransitions = self.create_priorTransition_dataframe(windows_mean, transition_score, priorCDRs)
-        hmmCDRpriors = self.combine_beds(priorCDRs, priorTransitions)
+        cdr_threshold = self.calculate_prior_percentiles(windows_mean)
+        prior_cdrs = self.create_prior_dataframe(self.merge_distance, windows_mean, cdr_threshold)
         
-        if hmmCDRpriors.empty:
-            print(f'No Priors Detected for {chrom} with settings: CDR Percentile - {self.prior_percent}, Transition Percentile - {self.prior_transition_percent}')
+        if prior_cdrs.empty:
+            print(f'No prior subCDRs Detected for {chrom} with percentile - {self.prior_percentile}')
             
             if self.retries < 25:
-                self.prior_percent = min(self.prior_percent + 1, 100)
-                self.prior_transition_percent = min(self.prior_transition_percent + 1, 100)
-                self.retries += 1
-                print(f'Retrying with CDR Percentile = {self.prior_percent}, Transition Percentile = {self.prior_transition_percent}')
-                print(f'This is retry attempt {self.retries}/25.')
-                return self.priors_single_chromosome(chrom, bed4Methyl_chrom)
-            raise RuntimeError(f"Failed to detect priors for {chrom} after {self.retries} retries with final settings: CDR Percentile - {self.prior_percent}, Transition Percentile - {self.prior_transition_percent}")
+                self.prior_percentile = min(self.prior_percentile + 1, 100)
 
-        return chrom, hmmCDRpriors
+                self.retries += 1
+                print(f'Retrying with Prior Percentile = {self.prior_percentile}')
+                print(f'This is retry attempt {self.retries}/25.')
+
+                return self.priors_single_chromosome(chrom, bed4Methyl_chrom)
+            raise RuntimeError(f"Failed to detect prior subCDRs for {chrom} after {self.retries} retries with final percentile - {self.prior_percentile}")
+
+        return chrom, prior_cdrs
 
     def priors_all_chromosomes(self, bed4Methyl_chrom_dict):
         hmmCDRpriors_chrom_dict = {}
@@ -132,12 +119,11 @@ def main():
     
     # hmmCDRprior Arguments
     argparser.add_argument('--window_size', type=int, default=1020, help='Window size to calculate prior regions. (default: 1020)')
-    argparser.add_argument('--prior_percent', type=int, default=5, help='Percentile for finding priorCDR regions. (default: 5)')
-    argparser.add_argument('--prior_transition_percent', type=int, default=10, help='Percentile for finding priorTransition regions. (default: 10)')
-    argparser.add_argument('--merge_distance', type=int, default=1021, help='Percentile for finding priorTransition regions. (default: 1021)')
+    argparser.add_argument('--prior_percentile', type=int, default=5, help='Percentile for finding CDR prior regions. (default: 5)')
+    argparser.add_argument('--merge_distance', type=int, default=1021, help='Distance to merge adjacent CDR regions. (default: 1021)')
     argparser.add_argument('--min_size', type=int, default=3000, help='Minimum size for CDR regions. (default: 3000)')
     argparser.add_argument('--enrichment', action='store_true', help='Enrichment flag. Pass in if you are looking for methylation enriched regions. (default: False)')
-    argparser.add_argument('--output_label', type=str, default='CDR', help='Label to use for name column of priorCDR BED file. (default: "CDR")')
+    argparser.add_argument('--output_label', type=str, default='subCDR', help='Label to use for name column of priorCDR BED file. (default: "subCDR")')
 
     argparser.add_argument('--save_intermediates', action='store_true', default=False, help="Set to true if you would like to save intermediates(filtered beds+window means). (default: False)")
     
@@ -152,14 +138,15 @@ def main():
             min_valid_cov=args.min_valid_cov
         )
 
-    bed4Methyl_chrom_dict, cenSat_chrom_dict = CDRparser.parse_all_chromosomes(bedMethyl_path=args.bedMethyl_path, 
-                                                                               cenSat_path=args.cenSat_path)
+    bed4Methyl_chrom_dict, cenSat_chrom_dict = CDRparser.process_files(
+        bedMethyl_path=args.bedMethyl_path, 
+        cenSat_path=args.cenSat_path
+    )
 
     priors = hmmCDRprior(
         window_size=args.window_size,
         min_size=args.min_size,
-        prior_percent=args.prior_percent,
-        prior_transition_percent=args.prior_transition_percent,
+        prior_percentile=args.prior_percentile,
         merge_distance=args.merge_distance, 
         enrichment=args.enrichment,
         output_label=args.output_label
