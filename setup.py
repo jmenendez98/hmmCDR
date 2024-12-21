de25d1d98430557d27b6353522a2976e7dd692ad
jmenendez98 <jmmenend@ucsc.edu>
2024-12-20 19:00:03 -0800

    updated dependancies to exlude python 3.13 and 3.14

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.8, <=3.12",
     extras_require={
         "dev": [
             "pytest",
